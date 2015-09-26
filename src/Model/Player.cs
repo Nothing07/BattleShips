a34c9edc74f451a69289651df9ceeb7d832bde34
Rodrigo36 <100608187@student.swin.edu.au>
2015-09-26 22:21:16 +1000

    Fixed counting issue
    Fixed counting issue in player.cs

--- src/Model/Player.cs
+++ src/Model/Player.cs
@@ -178,17 +178,20 @@ public class Player : IEnumerable<Ship>
 	/// <returns>the result of the attack</returns>
 	internal AttackResult Shoot(int row, int col)
 	{
-		_shots += 1;
 		AttackResult result = default(AttackResult);
 		result = EnemyGrid.HitTile(row, col);
 
 		switch (result.Value) {
 			case ResultOfAttack.Destroyed:
+			case ResultOfAttack.ShotAlready:
+				break;
 			case ResultOfAttack.Hit:
-				_hits += 1;
+				_hits++;
+				_shots++;
 				break;
 			case ResultOfAttack.Miss:
-				_misses += 1;
+				_misses++;
+				_shots++;
 				break;
 		}
 
